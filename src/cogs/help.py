89f114d76af9ab683ac202143e0bdbe247223d08
marvelman3284 <jedimaster3284@gmail.com>
2021-01-04 21:20:26 -0500

    Update help command

---
+++ src/cogs/help.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands
+
+
+class Help(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='help')
+    async def help(self, ctx, command="help"):
+        if command == "help":
+            help_embed = discord.Embed(
+                title="Help",
+                description="For more info on a catgeory run `help [category]`",
+                color=discord.Color.blurple
+            )
+            help_embed.add_field(name="Trivia", value="An RA themed competivie trivia game, with a scoreboard.")
+            help_embed.add_field(name="Utility", value="Utility Commands")
+            await ctx.send(embed=help_embed)
+
+        elif command == "trivia":
+            trivia_embed = discord.Embed(
+                title="Trivia Commands",
+                description="Help With Trivia Commands",
+                color=discord.Color.dark_gold
+            )
+            trivia_embed.add_field(name="trivia start", value="Starts a new game in the current channel")
+            trivia_embed.add_field(name="trivia stats", value="Returns your trivia win/loss statistics")
+            trivia_embed.add_field(name='trivia top', value="Sends an embed with the top 5 ranked users in trivia")
+            await ctx.send(embed=trivia_embed)
+
+        elif command == 'utils':
+            util_embed = discord.Embed(
+                title='Utility Commands',
+                description="Utility Commands",
+                color=discord.Color.darker_grey
+            )
+            util_embed.add_field(name="bofh", value="Convenient solutions to inconvenient tech problems")
+            util_embed.add_field(name="info", value="Information about the bot instance")
+            util_embed.add_field(name="ping", value="Displays latency between the client and the bot")
+            util_embed.add_field(name="roll", value="Rolls dice")
+            util_embed.add_field(name="userinfo", value="Displays information abouyt users")
+            util_embed.add_field(name="verify", value="Add yourself to the verified user role if the server, if you qualify.")
