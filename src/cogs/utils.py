de23cdf2a5e9e4701581c0d4bafa0348498f7c23
marvelman3284 <jedimaster3284@gmail.com>
2021-01-27 10:43:55 -0500

    Change ping doc string

--- src/cogs/utils.py
+++ src/cogs/utils.py
@@ -49,7 +49,7 @@ class Utils(commands.Cog):
 
     @commands.command()
     async def ping(self, ctx):
-        """Displays latency between client and bot"""
+        """The time it takes for disords servers to respond to Malcolm"""
         latency = round(self.bot.latency * 1000, 2)
         return await ctx.send('Pong! ' + str(latency) + 'ms')
 
